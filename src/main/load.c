a70c63c9551c06ae74ced8f5a10b94902e0a699b
2Tie <foxman95@ymail.com>
2026-02-09 09:51:15 -0700

    more load.c funcs, updated readme

--- src/main/load.c
+++ src/main/load.c
@@ -5,10 +5,17 @@ extern s32 load_bin0x1003b0(s32, u8*);
 extern int pl_motion_data0x2ebc50[];
 extern int em_motion_data0x2ec830[];
 extern int common_motion_data0x2ebc70[];
-extern u8* pl_area_top0x38a224;
 extern u8* data_load_ptr0x38a240;
 extern u8* stage_model0x38a220;
+extern u8* pl_area_top0x38a224;
 extern int stage_model_data0x2ec950[];
+extern int effect_model_data0x2ecee0[];
+extern int weapon_model_data0x2ec5b0[];
+extern int em_model_data0x2ec7a0[];
+extern int em_sub_model_data0x2ec8c0[];
+extern int npc_model_data0x2eced0[];
+extern int set_model_data0x2ecd70[];
+extern int edit_model_data0x386bd0[]; //actually extern, the rest should be in rodata?
 
 s32 load_file_mdl0x11ed20(u8* buff, s32 fileID) {
     if (fileID < 0) {
@@ -41,20 +48,63 @@ void load_stage_model0x11edf0(s32 stage) {
     amo_ahi_expand0x11f350(data_load_ptr0x38a240, model_ptr, model_ptr + 0x128000); //TODO this constant?
 }
 
-INCLUDE_ASM("asm/main/nonmatchings/load", load_eft_model0x11ee50);
+void load_eft_model0x11ee50(s32 eft) {
+    u8* model_ptr;
 
-INCLUDE_ASM("asm/main/nonmatchings/load", load_weapon_model0x11eeb0);
+    model_ptr = stage_model0x38a220;
+    load_file_mdl0x11ed20(data_load_ptr0x38a240, effect_model_data0x2ecee0[eft]);
+    amo_ahi_expand0x11f350(data_load_ptr0x38a240, model_ptr, model_ptr + 0x128000); //TODO this constant?
+}
+
+void load_weapon_model0x11eeb0(s32 weap) {
+    u8* model_ptr;
+
+    model_ptr = pl_area_top0x38a224;
+    load_file_mdl0x11ed20(data_load_ptr0x38a240, weapon_model_data0x2ec5b0[weap]);
+    amo_ahi_expand0x11f350(data_load_ptr0x38a240, model_ptr, model_ptr + 0x128000); //TODO this constant?
+}
 
 INCLUDE_ASM("asm/main/nonmatchings/load", load_armor_model0x11ef10);
 
-INCLUDE_ASM("asm/main/nonmatchings/load", load_enemy_model0x11ef90);
+void load_enemy_model0x11ef90(s32 enemy) {
+    u8* model_ptr;
 
-INCLUDE_ASM("asm/main/nonmatchings/load", load_enemy_sub_model0x11eff0);
+    model_ptr = pl_area_top0x38a224;
+    load_file_mdl0x11ed20(data_load_ptr0x38a240, em_model_data0x2ec7a0[enemy]);
+    amo_ahi_expand0x11f350(data_load_ptr0x38a240, model_ptr, model_ptr + 0x128000); //TODO this constant?
+}
 
-INCLUDE_ASM("asm/main/nonmatchings/load", load_npc_model0x11f050);
+void load_enemy_sub_model0x11eff0(s32 enemy) {
+    u8* model_ptr;
 
-INCLUDE_ASM("asm/main/nonmatchings/load", load_set_model0x11f0b0);
+    model_ptr = pl_area_top0x38a224;
+    load_file_mdl0x11ed20(data_load_ptr0x38a240, em_sub_model_data0x2ec8c0[enemy]);
+    amo_ahi_expand0x11f350(data_load_ptr0x38a240, model_ptr, model_ptr + 0x128000); //TODO this constant?
+}
 
+void load_npc_model0x11f050(s32 npc) {
+    u8* model_ptr;
+
+    model_ptr = pl_area_top0x38a224;
+    load_file_mdl0x11ed20(data_load_ptr0x38a240, npc_model_data0x2eced0[npc]);
+    amo_ahi_expand0x11f350(data_load_ptr0x38a240, model_ptr, model_ptr + 0x128000); //TODO this constant?
+}
+
+void load_set_model0x11f0b0(s32 stage) {
+    u8* model_ptr;
+
+    model_ptr = stage_model0x38a220;
+    load_file_mdl0x11ed20(data_load_ptr0x38a240, set_model_data0x2ecd70[stage]);
+    amo_ahi_expand0x11f350(data_load_ptr0x38a240, model_ptr, model_ptr + 0x128000); //TODO this constant?
+}
+
+/*void load_edit_model0x11f110(s32 edit) {
+    u8* model_ptr;
+
+    model_ptr = pl_area_top0x38a224;
+    load_file_mdl0x11ed20(data_load_ptr0x38a240, edit_model_data0x386bd0[edit]);
+    amo_ahi_expand0x11f350(data_load_ptr0x38a240, model_ptr, model_ptr + 0x128000); //TODO this constant?
+}*/
 INCLUDE_ASM("asm/main/nonmatchings/load", load_edit_model0x11f110);
 
 void debug_model_load0x11f160(void) {
