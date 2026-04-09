742ef8006c8d43b8ed2a2ea91caf73a25a261088
2Tie <foxman95@ymail.com>
2026-04-08 18:01:20 -0700

    prim.c: matched get and release funcs

--- src/main/prim.c
+++ src/main/prim.c
@@ -1,6 +1,11 @@
 #include "common.h"
 #include "structs.h"
 
+extern void flmatrLoad0x173490(f32[], s32);
+extern void flvecApplyMat0x172ee0(f32[], f32[], f32[]);
+extern void plplAdd0x194990(prim*, f32*);
+extern void plplAdd20x1691c0(prim*, f32*);
+
 extern prim prim0x3eddf0[0x200]; //migrate these when the file's done
 extern prim prim20x3ebdf0[0x100];
 extern prim pit_prim0x3ebd70[4];
@@ -48,13 +53,69 @@ prim* get_prim_ptr20x169320(s32 which) {
     return pointer;
 }
 
-INCLUDE_ASM("asm/main/nonmatchings/prim", get_prim0x169340);
+s16 get_prim0x169340(void) {
+    prim *ptr;
+    s32 top;
+    s32 saved;
 
-INCLUDE_ASM("asm/main/nonmatchings/prim", get_prim20x1693d0);
+    saved = prim_free_top0x38a190;
+    if (saved >= 0x200)
+        return -1;
+    ptr = get_prim_ptr0x169300(saved + 1);
+    top = prim_free_top0x38a190 + 1;
+    while(top < 0x200) {
+        if (ptr[0][5] == 0) {
+            break;
+        }
+        top += 1;
+        ptr += 1;
+    }
+    prim_free_top0x38a190 = top;
+    return saved;
+}
 
-INCLUDE_ASM("asm/main/nonmatchings/prim", release_prim0x169460);
+s16 get_prim20x1693d0(void) {
+    s32 saved;
+    s32 top;
+    prim* ptr;
 
-INCLUDE_ASM("asm/main/nonmatchings/prim", release_prim20x1694b0);
+    saved = prim_free_top20x38a18c;
+    if (saved >= 0x100)
+        return -1;
+    ptr = get_prim_ptr20x169320(saved + 1);
+    top = prim_free_top20x38a18c + 1;
+    while(top < 0x100) {
+        if (ptr[0][5] == 0)
+            break;
+        top += 1;
+        ptr += 1;
+    }
+    prim_free_top20x38a18c = top;
+    return saved;
+}
+
+void release_prim0x169460(s32 which) {
+    memset0x19dd28(get_prim_ptr0x169300(which), 0, sizeof(prim));
+    if (which < prim_free_top0x38a190) {
+        prim_free_top0x38a190 = which;
+    }
+}
+
+void release_prim20x1694b0(s32 which) {
+    prim* ptr;
+    s32 six;
+    s32 seven;
+
+    ptr = get_prim_ptr20x169320(which);
+    six = (*ptr)[6];
+    seven = (*ptr)[7];
+    memset0x19dd28(ptr, 0, sizeof(prim));
+    (*ptr)[6] = six;
+    (*ptr)[7] = seven;
+    if (which < prim_free_top20x38a18c) {
+        prim_free_top20x38a18c = which;
+    }
+}
 
 INCLUDE_ASM("asm/main/nonmatchings/prim", add_prim0x169530);
 
