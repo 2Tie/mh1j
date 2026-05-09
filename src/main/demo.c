9b8a57215d2caba6c4da61ad98c36a93fca94913
2Tie <foxman95@ymail.com>
2026-05-09 09:19:12 -0700

    demo.c: matched EvDemoInitialize

--- src/main/demo.c
+++ src/main/demo.c
@@ -9,10 +9,23 @@ extern void stage_bgm_set0x21df80(u8);
 extern void DemoCameraCancel0x221ba0(void);
 extern s32 DemoCameraCheck0x221bd0();
 extern s32 Event_flag_set0x272ec0(s32);
+extern void Event_flag_clear0x272f00(s32);
 extern s32 Event_flag_ck0x272f50(s32);
 extern bool Tutorial_flag_set0x63afa0(u8);
 
-
+extern EVENT_DEMO_DATA evdemo_030x355460;
+extern EVENT_DEMO_DATA evdemo_040x355470;
+extern EVENT_DEMO_DATA evdemo_050x355480;
+extern EVENT_DEMO_DATA evdemo_060x355490;
+extern EVENT_DEMO_DATA evdemo_080x3554a0;
+extern EVENT_DEMO_DATA evdemo_100x3554b0;
+extern EVENT_DEMO_DATA evdemo_110x3554c0;
+extern EVENT_DEMO_DATA evdemo_140x3554d0;
+extern EVENT_DEMO_DATA evdemo_170x3554e0;
+extern EVENT_DEMO_DATA evdemo_200x3554f0;
+extern EVENT_DEMO_DATA evdemo_230x355500;
+extern EVENT_DEMO_DATA evdemo_240x355510;
+extern EVENT_DEMO event_demo0x531910;
 extern u8 player_work0x3e4bf0[];
 extern GAME_WORK game_w0x3f33f0;
 
@@ -22,7 +35,60 @@ extern GAME_WORK game_w0x3f33f0;
 s32 evdemo_init_sub0x286510(ACTIVE_EVENT_DEMO* arg0, EVENT_DEMO_DATA* arg1);
 void evdemo_camera_request0x286920(u8, u8);
 
-INCLUDE_ASM("asm/main/nonmatchings/demo", EvDemoInitialize0x2862f0);
+void EvDemoInitialize0x2862f0(void) {
+    u16 quest;
+    EVENT_DEMO* event = &event_demo0x531910;
+    
+    game_w0x3f33f0.demo_playing = 0;
+    event_demo0x531910.active = 0;
+    *(s32*)&event_demo0x531910.demos[1].active = 0;
+    *(s32*)&event_demo0x531910.demos[0].active = 0;
+    quest = game_w0x3f33f0.selected_quest_id;
+
+    switch (quest) {
+    case 0x88:
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_030x355460);
+        return;
+    case 0x89:
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_040x355470);
+        return;
+    case 0x8A:
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_050x355480);
+        evdemo_init_sub0x286510(&event->demos[1], &evdemo_240x355510);
+        return;
+    case 0x8B:
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_060x355490);
+        return;
+    case 0x87:
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_080x3554a0);
+        return;
+    case 0x94:
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_100x3554b0);
+        return;
+    case 0x9A:
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_110x3554c0);
+        return;
+    case 0xAB:
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_140x3554d0);
+        return;
+    case 0x65:
+    case 0x6B:
+    case 0xCE:
+        Event_flag_clear0x272f00(0x18);
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_170x3554e0);
+        return;
+    case 0x67:
+    case 0x68:
+    case 0x69:
+    case 0x6A:
+    case 0xCF:
+        Event_flag_clear0x272f00(0x1B);
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_200x3554f0);
+        return;
+    case 0x83:
+        evdemo_init_sub0x286510(&event->demos[0], &evdemo_230x355500);
+    }
+}
 
 s32 evdemo_init_sub0x286510(ACTIVE_EVENT_DEMO* demo, EVENT_DEMO_DATA* data) {
     if (Event_flag_ck0x272f50(data->flag) == 0) {
